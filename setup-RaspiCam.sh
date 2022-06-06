278bb09b4aa0b6e31b22d80a51c7d0f16f2232bf
vonschnabel <77418193+vonschnabel@users.noreply.github.com>
2022-06-06 18:41:20 +0200

    Add files via upload

---
+++ setup-RaspiCam.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]; then
+    echo "Please run as root"
+    exit 1
+fi
+
+echo "Install RaspiCam";
+echo "";
+echo "####################################";
+echo "";
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/RaspiCam"
+if [ -d "$DIR" ]; then
+  echo "Directory RaspiCam found. Proceeding with installation..."
+else
+  echo "Directory RaspiAP not found. Cloning from Github..."
+  git clone https://github.com/vonschnabel/RaspiCam.git
+fi
+
+sudo apt install apache2 php php-mbstring libapache2-mod-php python-picamera python3-picamera -y
+
+#sudo sed -i 's/Priv/#Priv/g' /lib/systemd/system/apache2.service
+
+sudo mv ./RaspiCam/070-raspicam /etc/sudoers.d/
+sudo chown root:root /etc/sudoers.d/070-raspicam
+sudo chmod 440 /etc/sudoers.d/070-raspicam
+
+sudo mv ./RaspiCam/stream_svc.py /usr/local/bin/
+sudo mv ./RaspiCam/stream_record_svc.py /usr/local/bin
+sudo mv ./RaspiCam/webstream.conf /usr/local/bin
+sudo chmod +x /usr/local/bin/stream_svc.py
+sudo chmod +x /usr/local/bin/stream_record_svc.py
+
+sudo mv ./RaspiCam/cam.php /var/www/html/
+sudo mv ./RaspiCam/camfunctions.php /var/www/html/
+sudo mv ./RaspiCam/videocaptures.php /var/www/html/
+sudo mv ./RaspiCam/jquery.min.js /var/www/html/
+sudo mv ./RaspiCam/w3.css /var/www/html/
+
+rm -rf RaspiCam/
+echo "";
+echo "Installation complete";
